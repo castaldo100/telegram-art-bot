2d4b496f583ac0e900c676857549ad769252bf66
Holger Kurtz <kulturdata@gmail.com>
2020-07-03 17:47:35 +0200

    Erste Funktionen

--- bot.py
+++ bot.py
@@ -5,25 +5,61 @@
 import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-Token = '1282912519:AAE2gkIO8pKAsqRKCficQRNWcW0haM_7-7k'
 # Token muss später noch als JSON geladen werden, um nicht öffentlich zu sein
+Token = '1282912519:AAE2gkIO8pKAsqRKCficQRNWcW0haM_7-7k'
 
-
+# Grundlegendes
 bot = telegram.Bot(token=Token)
 updater = Updater(token=Token, use_context=True)
 dispatcher = updater.dispatcher
 
+# Erstes Command
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, 
-    text="I'm a bot, please talk to me!")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text="I'm a bot, please talk to me!"
+        )
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+# Für reguläre Nachrichten
+def echo(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text=update.message.text
+        )
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text=text_caps
+        )
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+# Geht mit unbekannten Commands um
+# Muss am Ende stehen
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
 # Startet das Bot
 updater.start_polling()
+# Script stoppen mit ctrl + c
+updater.idle() # wartet auf ctrl + c 
+
+
 
